3d3e33990bbe0b18d6b2058b33ad1484b37828ce
Vikas Lakka <vikaslakkacs@gmail.com>
2022-09-02 14:46:11 +0530

    crated exception and logger

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup 
+from setuptools import setup, find_packages
 from typing import List
 ##Declaring variabled for setup
 PROJECT_NAME="credit-card-defaulter"
@@ -16,6 +16,7 @@ def get_requirements_list()->List[str]:
     """
     with open(REQUIREMENTS_FILE_NAME,"r") as requirements:
         return requirements.readlines().remove("-e .")
+        
 
 ## Defining the variables for setup function
 setup(
@@ -23,6 +24,6 @@ setup(
     version=VERSION,
     author=AUTHOR,
     description="Project to find out defaulters of credit card by given data",
-    packages=PACKAGES,## Specify the root package to install
+    packages=find_packages(),## Specify the root package to install
     install_requires=get_requirements_list()## Function returns list of all packages to install
 )
